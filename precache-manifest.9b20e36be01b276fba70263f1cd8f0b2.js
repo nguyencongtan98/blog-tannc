3daad91a4d01bd69b2c86e8da8456fe62d6d66bd
Nguyen Cong Tan <tancoder01@gmail.com>
2020-10-07 20:52:32 +0700

    Updates

---
+++ precache-manifest.9b20e36be01b276fba70263f1cd8f0b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff45328c04444a4a32a7c97b8ce0b987",
+    "url": "/blog-tannc/index.html"
+  },
+  {
+    "revision": "ec9690aee9946c59f4d7",
+    "url": "/blog-tannc/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3bd71b05b772a8803017",
+    "url": "/blog-tannc/static/js/2.86c23e58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blog-tannc/static/js/2.86c23e58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec9690aee9946c59f4d7",
+    "url": "/blog-tannc/static/js/main.a77e4790.chunk.js"
+  },
+  {
+    "revision": "41e708ab9bba5ed08dee",
+    "url": "/blog-tannc/static/js/runtime-main.6a54f863.js"
+  }
+]);
